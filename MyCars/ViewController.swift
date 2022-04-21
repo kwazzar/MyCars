c6ee60acceca6a67ce4b8169bc05bbf50fae5184
kwazzar <fugitiwe@gmail.com>
2022-04-21 17:49:39 +0300

    Initial Commit

---
+++ MyCars/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCars
+//
+//  Created by Quasar on 21.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
